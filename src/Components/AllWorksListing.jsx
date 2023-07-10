c9e0aa0bc595478e71c7614a904ba515bd6813da
Adhars <138186421+Adharsraj@users.noreply.github.com>
2023-07-10 19:13:34 +0530

    underline

--- src/Components/AllWorksListing.jsx
+++ src/Components/AllWorksListing.jsx
@@ -10,7 +10,7 @@ const AllWorksListing = () => {
       <div className="grid grid-cols-2 gap-4 mb-10 ">
         <div className="flex items-center justify-center md:justify-between lg:ml-40 md:ml-20">
           <div className=" mr-5 border-b-2 lg:text-xl">
-            <h1 className="text-lg underline lg:underline-offset-0 md:text-xl mb-4 font-times ">{m.title}</h1>
+            <h1 className="text-lg underline  md:text-xl mb-4 font-times ">{m.title}</h1>
             <h1 className="font-dm lg:text-xl">{m.head1}</h1>
           </div>
         </div>
