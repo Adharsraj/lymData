b62dba754f63b10242e5ef7ae3776025ee0f4431
Adhars <138186421+Adharsraj@users.noreply.github.com>
2023-08-05 22:54:53 +0530

    fix/new

--- dist/assets/TestskeltandLoader-a13413e8.js
+++ dist/assets/TestskeltandLoader-a13413e8.js
@@ -1,5 +1,1 @@
-<<<<<<<< HEAD:dist/assets/TestskeltandLoader-a13413e8.js
 import{u as s,j as t}from"./index-a26532e6.js";const o=()=>{const e=s();return console.log(e),t.jsx("div",{children:"TestskeltandLoader"})};export{o as default};
-========
-import{u as s,j as t}from"./index-c44d9ae5.js";const o=()=>{const e=s();return console.log(e),t.jsx("div",{children:"TestskeltandLoader"})};export{o as default};
->>>>>>>> f8232eed769ddae83a8446ed7537d08d919e125f:dist/assets/TestskeltandLoader-98a5c3c8.js
--- dist/assets/UseDocumentTitle-d85d88ba.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-c44d9ae5.js";const r=t=>{e.useEffect(()=>{document.title=t},[t])};export{r as U};
