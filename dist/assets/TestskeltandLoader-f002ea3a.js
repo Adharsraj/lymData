33f86b93e9ae7201af14077dd5779d11b7900ea0
Adhars <138186421+Adharsraj@users.noreply.github.com>
2023-08-05 15:11:45 +0530

    fix/cursor

--- dist/assets/TestskeltandLoader-f002ea3a.js
+++
@@ -1,1 +0,0 @@
-import{u as s,j as t}from"./index-af4c9557.js";const o=()=>{const e=s();return console.log(e),t.jsx("div",{children:"TestskeltandLoader"})};export{o as default};
--- dist/assets/UseDocumentTitle-91598a68.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-af4c9557.js";const r=t=>{e.useEffect(()=>{document.title=t},[t])};export{r as U};
