5147d8d5ce537de95c00734d20fbdd2d470dbdc3
Adhars <138186421+Adharsraj@users.noreply.github.com>
2023-08-05 11:40:59 +0530

    fix/alhub

---
+++ dist/assets/TestskeltandLoader-7b2abc4a.js
@@ -0,0 +1,1 @@
+import{u as s,j as t}from"./index-fa6ec69f.js";const o=()=>{const e=s();return console.log(e),t.jsx("div",{children:"TestskeltandLoader"})};export{o as default};
---
+++ dist/assets/UseDocumentTitle-f4d5c5cb.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-fa6ec69f.js";const r=t=>{e.useEffect(()=>{document.title=t},[t])};export{r as U};
