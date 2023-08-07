59cd976e47105f47693b2af49903980da885acdf
Adhars <138186421+Adharsraj@users.noreply.github.com>
2023-08-07 10:48:49 +0530

    fix/buttons

---
+++ dist/assets/TestskeltandLoader-9b9e26a8.js
@@ -0,0 +1,1 @@
+import{u as s,j as t}from"./index-1f7db3dc.js";const o=()=>{const e=s();return console.log(e),t.jsx("div",{children:"TestskeltandLoader"})};export{o as default};
--- dist/assets/UseDocumentTitle-7bc391f2.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-a26532e6.js";const r=t=>{e.useEffect(()=>{document.title=t},[t])};export{r as U};
