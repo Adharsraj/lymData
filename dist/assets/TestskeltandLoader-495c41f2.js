21fd6e1434d36dc10bc1ba5b4acaa54b7bfae800
Jifhnanazar <jifhnapn@lymdata.com>
2023-08-07 13:56:56 +0530

    Latest

---
+++ dist/assets/TestskeltandLoader-495c41f2.js
@@ -0,0 +1,1 @@
+import{u as s,j as t}from"./index-9edf48fd.js";const o=()=>{const e=s();return console.log(e),t.jsx("div",{children:"TestskeltandLoader"})};export{o as default};
--- dist/assets/UseDocumentTitle-9bf9c8f4.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-0c07bf92.js";const r=t=>{e.useEffect(()=>{document.title=t},[t])};export{r as U};
