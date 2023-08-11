3e492d157d11b2cacbdfa864a1351e5b56618bfa
Adhars <138186421+Adharsraj@users.noreply.github.com>
2023-08-11 14:42:48 +0530

    fix/loader

---
+++ dist/assets/UseDocumentTitle-9234eaea.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-83af5778.js";const r=t=>{e.useEffect(()=>{document.title=t},[t])};export{r as U};
--- dist/assets/UseDocumentTitle-acc1ea44.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-aadc8e74.js";const r=t=>{e.useEffect(()=>{document.title=t},[t])};export{r as U};
