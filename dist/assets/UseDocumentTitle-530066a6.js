f469f5aeec73da5afb16a751f9a7753541959a1f
Jifhnanazar <jifhnapn@lymdata.com>
2023-09-27 11:17:09 +0530

    latest

--- dist/assets/UseDocumentTitle-530066a6.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-62c177e3.js";const r=t=>{e.useEffect(()=>{document.title=t},[t])};export{r as U};
---
+++ dist/assets/UseDocumentTitle-9c784adc.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-f155c0c5.js";const r=t=>{e.useEffect(()=>{document.title=t},[t])};export{r as U};
