f4027c4acace84d8d932e1236b1b80618e1bf7c1
Jifhnanazar <138013434+Jifhnanazar@users.noreply.github.com>
2023-11-01 15:27:14 +0530

    Blog

--- dist/assets/UseDocumentTitle-a1a3a4e5.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-16a1bcae.js";const r=t=>{e.useEffect(()=>{document.title=t},[t])};export{r as U};
--- src/Components/Blog/BlogCards.jsx
+++ src/Components/Blog/BlogCards.jsx
@@ -8,11 +8,11 @@ const BlogCards = () => {
   <div className='grid  p-5  gap-7 md:grid-cols-2 lg:grid-cols-4 lg:px-10'>
 
   {blogCards.map((m, index) => (
-    <div className='w-full lg:w-[300px] lg:mt-10 h-[250px] bg-white cursor-pointer rounded-3xl mx-auto shadow-2xl'>
-  <img src={m.img} className='h-[56px] w-full rounded-t-3xl' alt="" />
+    <div className='w-full lg:w-[300px] lg:mt-10 h-[450px] bg-white cursor-pointer rounded-3xl mx-auto shadow-2xl'>
+  <img src={m.img} className='h-[190px] w-full rounded-t-3xl' alt="" />
   <div className='flex flex-col justify-between h-[160px] lg:px-5 px-3 pt-2 lg:pt-3 some-element'>
-  <h1 className='font-bold lg:mt-10'>{m.title}</h1>
-  <h2 className='lg:text-[14px] text-[12px] lg:mt-0'>{m.desc}</h2>
+  <h1 className='font-bold lg:mt-5'>{m.title}</h1>
+  <h2 className='lg:text-[14px] text-[12px] lg:mt-20'>{m.desc}</h2>
 
   </div>
     </div>
