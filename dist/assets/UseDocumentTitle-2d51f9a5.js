2dcd95d2c7b1798c8749504d2789ea5991ba8c55
Jifhnanazar <138013434+Jifhnanazar@users.noreply.github.com>
2023-11-10 15:37:05 +0530

    footer

---
+++ dist/assets/UseDocumentTitle-2d51f9a5.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-b9bf3dda.js";const r=t=>{e.useEffect(()=>{document.title=t},[t])};export{r as U};
--- dist/assets/UseDocumentTitle-902bb986.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-a596c98b.js";const r=t=>{e.useEffect(()=>{document.title=t},[t])};export{r as U};
