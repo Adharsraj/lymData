5571c1fbe2aa0de42d5127ef347c8f0845abc787
Adharsraj <138186421+Adharsraj@users.noreply.github.com>
2024-01-08 11:10:19 +0530

    ins

---
+++ dist/assets/UseDocumentTitle-6d436353.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-0bd5191d.js";const r=t=>{e.useEffect(()=>{document.title=t},[t])};export{r as U};
--- dist/assets/UseDocumentTitle-ca6e546e.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-07568a85.js";const r=t=>{e.useEffect(()=>{document.title=t},[t])};export{r as U};
